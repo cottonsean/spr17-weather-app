82556041f92701c0b8a0cde5b0270a2092bb1ec4
Heather Creighton <heather.creighton@gmail.com>
2017-04-25 09:46:24 -0400

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,24 @@
+class WelcomeController < ApplicationController
+  def test
+  	response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/geolookup/conditions/q/MI/Detroit.json")
+  
+puts "-----------BEGIN OF RESPONSE--------------"
+puts response
+puts "-----------END OF RESPONSE--------------"
+
+
+  @location = response['location']['city']
+  @temp_f = response['current_observation']['temp_f']
+  @temp_c = response['current_observation']['temp_c']
+  @weather_icon = response['current_observation']['icon_url']
+  @weather_words = response['current_observation']['weather'] 
+  @forecast_link = response['current_observation']['forecast_url']
+  @real_feel = response['current_observation']['feelslike_f']
+
+
+  end
+
+  def index
+
+  end
+end
